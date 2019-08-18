af3106d3ce5c25d8d18a9857c50099fecd34eb0b
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-08-18 10:14:11 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(know_how)
+        @knowledge << know_how
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
+
 
 end
