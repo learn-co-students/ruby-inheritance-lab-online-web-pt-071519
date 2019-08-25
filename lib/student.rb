55578b7a2c5832d57f51f12aecaa7a91dd6394f4
Renee Sims <info@reneesoul.com>
2019-08-25 00:28:27 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge, :learn
+ 
+  def initialize
+    @knowledge = []
+  end 
 
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    return @knowledge
+    
+  end
 end
